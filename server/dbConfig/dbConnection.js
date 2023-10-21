e68834d64b76474c68db2170c5e3d67fc8a700f4
Shemeer <shameerbem@gmail.com>
2023-10-21 13:08:17 +0530

    updated commit

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -2,7 +2,7 @@ const mysql2 = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql2.createConnection({
-    host:"Shemeer@127.0.0.1:3306",
+    host:"127.0.0.1:3306",
     user:"Shemeer",
     password:process.env.PASSWORD,
     database:'snapqr'
